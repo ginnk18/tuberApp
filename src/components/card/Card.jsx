e9082c0779e210a1b5bba456d768da0d27a657a7
Jeffrey <Jmmercer@ucalgary.ca>
2017-03-09 17:17:13 +0000

    result rate

--- src/components/card/Card.jsx
+++ src/components/card/Card.jsx
@@ -23,7 +23,8 @@ class Card extends Component {
                   <a href="#0" onClick={() => store.dispatch(profileActions.loadProfile(this.props.tutor.id)) } >
                     <strong className="name">{this.props.tutor.name}</strong><br/>
                   </a>
-                  <span className="tutor-attr">Subjects:&nbsp;&nbsp;</span><span>{this.props.tutor.subjects}</span><br/>
+                  <span className="tutor-attr">Rate:&nbsp;&nbsp;</span><span>${this.props.tutor.rate_cents / 100}</span><br/>
+                  <span className="tutor-attrSub">Subjects:&nbsp;&nbsp;</span><span>{this.props.tutor.subjects}</span><br/>
                   <span className="tutor-attr">Qualification:&nbsp;&nbsp;</span><span>{this.props.tutor.education}</span><br/>
                   <span className="tutor-attr">Experience:&nbsp;&nbsp;</span><span>{this.props.tutor.experience}</span>
                   <div className="card-btn">
