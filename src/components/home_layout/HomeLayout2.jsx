3d992d1e31b37712ac2284e7663f8102efd622dc
Jeffrey <Jmmercer@ucalgary.ca>
2017-03-09 05:09:48 +0000

    pre-pres

--- src/components/home_layout/HomeLayout2.jsx
+++ src/components/home_layout/HomeLayout2.jsx
@@ -72,9 +72,9 @@ class HomeLayout extends Component {
         <section className="tagline">
           <h1><span className="brand">tuber&nbsp;</span>
             <span className="tagline-text">painlessly connects students with
-            tutors.{/*<HeaderDropdown />*/}</span>
+            tutors.<HeaderDropdown /></span>
           </h1>
-          <HeaderDropdown />
+          {/*<HeaderDropdown />*/}
           <div className="styleButton">
           </div>
           </section>
